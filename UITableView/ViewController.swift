396aca006aa1a7dbd5ea77301b91da2fbc85fd59
Mako0722 <zhongtiancheng79@gmail.com>
2022-11-12 00:08:50 +0900

    UITableVie

--- UITableView/ViewController.swift
+++ UITableView/ViewController.swift
@@ -7,13 +7,49 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    var tableView: UITableView?
+    let items = ["Apple", "Banana", "Orange"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+       
+        self.tableView = {
+            let tableView = UITableView(frame: self.view.bounds, style: .plain)
+            tableView.autoresizingMask = [
+                .flexibleWidth,
+                .flexibleHeight
+            ]
+            tableView.delegate = self
+            tableView.dataSource = self
+            
+            self.view.addSubview(tableView)
+            
+            return tableView
+        }()
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
     }
 
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+      return self.items.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+      let cell = tableView.dequeueReusableCell(withIdentifier: "Cell")
+      ?? UITableViewCell(style: .default, reuseIdentifier: "Cell")
 
+      cell.textLabel?.text = self.items[indexPath.row]
+
+      return cell
+
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+      print("Selected! \(self.items[indexPath.row])")
+    }
 }
 
